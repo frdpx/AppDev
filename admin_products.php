c60f9dd57064f1ca6ad470e73c8b295fe6bfbeca
YADAPAW <yada.paw@ku.th>
2024-03-31 23:29:58 +0700

    product

--- admin_products.php
+++ admin_products.php
@@ -18,4 +18,16 @@ if(!isset($admin_id)){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>products</title>
+   <title></title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- custom admin css file link  -->
+   <link rel="stylesheet" href="css/admin_style.css">
+
+   </head>
+<body>
+   
+<?php include 'admin_header.php'; ?>
+
