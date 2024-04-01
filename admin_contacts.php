87a3ccbc0858a13336d06e85290b69e3642a1f28
yada paw <yada.paw@ku.th>
2024-04-01 16:11:27 +0700

    message

--- admin_contacts.php
+++ admin_contacts.php
@@ -36,4 +36,34 @@ if(isset($_GET['delete'])){
 <body>
    
 
+<?php include 'admin_header.php'; ?>
+
+<section class="messages">
+
+   <h1 class="title"> messages </h1>
+
+   <div class="box-container">
+   <?php
+      $select_message = mysqli_query($conn, "SELECT * FROM `message`") or die('query failed');
+      if(mysqli_num_rows($select_message) > 0){
+         while($fetch_message = mysqli_fetch_assoc($select_message)){
+      
+   ?>
+   <div class="box">
+      <p> user id : <span><?php echo $fetch_message['user_id']; ?></span> </p>
+      <p> name : <span><?php echo $fetch_message['name']; ?></span> </p>
+      <p> number : <span><?php echo $fetch_message['number']; ?></span> </p>
+      <p> email : <span><?php echo $fetch_message['email']; ?></span> </p>
+      <p> message : <span><?php echo $fetch_message['message']; ?></span> </p>
+      <a href="admin_contacts.php?delete=<?php echo $fetch_message['id']; ?>" onclick="return confirm('delete this message?');" class="delete-btn">delete message</a>
+   </div>
+   <?php
+      };
+   }else{
+      echo '<p class="empty">you have no messages!</p>';
+   }
+   ?>
+   </div>
+
+</section>
 
