f71510ec5c029c8f4ba2c28b57e830ad4203d01a
yada paw <yada.paw@ku.th>
2024-04-01 16:02:09 +0700

    เพิ่ม admin_header

--- admin_users.php
+++ admin_users.php
@@ -29,3 +29,27 @@ if(!isset($admin_id)){
 </head>
 <body>
    
+<?php include 'admin_header.php'; ?>
+
+<section class="users">
+
+   <h1 class="title"> user accounts </h1>
+
+   <div class="box-container">
+      <?php
+         $select_users = mysqli_query($conn, "SELECT * FROM `users`") or die('query failed');
+         while($fetch_users = mysqli_fetch_assoc($select_users)){
+      ?>
+      <div class="box">
+         <p> user id : <span><?php echo $fetch_users['id']; ?></span> </p>
+         <p> username : <span><?php echo $fetch_users['name']; ?></span> </p>
+         <p> email : <span><?php echo $fetch_users['email']; ?></span> </p>
+         <p> user type : <span style="color:<?php if($fetch_users['user_type'] == 'admin'){ echo 'var(--orange)'; } ?>"><?php echo $fetch_users['user_type']; ?></span> </p>
+         <a href="admin_users.php?delete=<?php echo $fetch_users['id']; ?>" onclick="return confirm('delete this user?');" class="delete-btn">delete user</a>
+      </div>
+      <?php
+         };
+      ?>
+   </div>
+
+</section>
