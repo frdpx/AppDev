630af96bd04693d08b9de9c9d23bfc691742b206
Potaetnbe <tanabodee.kl@ku.th>
2024-04-01 16:24:14 +0700

    display-order

--- checkout.php
+++ checkout.php
@@ -36,6 +36,86 @@ if(!isset($user_id)){
    <p> <a href="home.php">home</a> / checkout </p>
 </div>
 
+<section class="display-order">
+
+   <?php  
+      $grand_total = 0;
+      $select_cart = mysqli_query($conn, "SELECT * FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
+      if(mysqli_num_rows($select_cart) > 0){
+         while($fetch_cart = mysqli_fetch_assoc($select_cart)){
+            $total_price = ($fetch_cart['price'] * $fetch_cart['quantity']);
+            $grand_total += $total_price;
+   ?>
+   <p> <?php echo $fetch_cart['name']; ?> <span>(<?php echo '$'.$fetch_cart['price'].'/-'.' x '. $fetch_cart['quantity']; ?>)</span> </p>
+   <?php
+      }
+   }else{
+      echo '<p class="empty">your cart is empty</p>';
+   }
+   ?>
+<div class="grand-total"> grand total : <span>$<?php echo $grand_total; ?>/-</span> </div>
+
+</section>
+
+<section class="checkout">
+
+   <form action="" method="post">
+      <h3>place your order</h3>
+      <div class="flex">
+         <div class="inputBox">
+            <span>your name :</span>
+            <input type="text" name="name" required placeholder="enter your name">
+         </div>
+         <div class="inputBox">
+            <span>your number :</span>
+            <input type="number" name="number" required placeholder="enter your number">
+         </div>
+         <div class="inputBox">
+            <span>your email :</span>
+            <input type="email" name="email" required placeholder="enter your email">
+         </div>
+         <div class="inputBox">
+            <span>payment method :</span>
+            <select name="method">
+               <option value="cash on delivery">cash on delivery</option>
+               <option value="credit card">credit card</option>
+               <option value="paypal">paypal</option>
+               <option value="paytm">paytm</option>
+            </select>
+         </div>
+         <div class="inputBox">
+            <span>address line 01 :</span>
+            <input type="number" min="0" name="flat" required placeholder="e.g. flat no.">
+         </div>
+         <div class="inputBox">
+            <span>address line 01 :</span>
+            <input type="text" name="street" required placeholder="e.g. street name">
+         </div>
+         <div class="inputBox">
+            <span>city :</span>
+            <input type="text" name="city" required placeholder="e.g. mumbai">
+         </div>
+         <div class="inputBox">
+            <span>state :</span>
+            <input type="text" name="state" required placeholder="e.g. maharashtra">
+         </div>
+         <div class="inputBox">
+            <span>country :</span>
+            <input type="text" name="country" required placeholder="e.g. india">
+         </div>
+         <div class="inputBox">
+            <span>pin code :</span>
+            <input type="number" min="0" name="pin_code" required placeholder="e.g. 123456">
+         </div>
+      </div>
+      <input type="submit" value="order now" class="btn" name="order_btn">
+   </form>
+
+</section>
+
+
+
+
 
 
 
